f7538c4267f0e0a2fbf2a4545e1fe65121139c9a
crykn <crykn@users.noreply.github.com>
2020-10-23 23:57:28 +0200

    Fix the ordering of render() calls

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/particleeditor/ParticleEditor.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/particleeditor/ParticleEditor.java
@@ -104,61 +104,57 @@ public class ParticleEditor extends JFrame {
 	}
 
 	void reloadRows () {
-		EventQueue.invokeLater(new Runnable() {
-			public void run () {
-				editRowsPanel.removeAll();
-				addEditorRow(new NumericPanel(pixelsPerMeter, "Pixels per meter", ""));
-				addEditorRow(new NumericPanel(zoomLevel, "Zoom level", ""));
-				addEditorRow(new NumericPanel(deltaMultiplier, "Delta multiplier", ""));
-				addEditorRow(new GradientPanel(backgroundColor, "Background color", "", true));
+		editRowsPanel.removeAll();
+		addEditorRow(new NumericPanel(pixelsPerMeter, "Pixels per meter", ""));
+		addEditorRow(new NumericPanel(zoomLevel, "Zoom level", ""));
+		addEditorRow(new NumericPanel(deltaMultiplier, "Delta multiplier", ""));
+		addEditorRow(new GradientPanel(backgroundColor, "Background color", "", true));
 
-				previewImagePanel = new PreviewImagePanel(ParticleEditor.this, "Preview Image", "");
-				addEditorRow(previewImagePanel);
+		previewImagePanel = new PreviewImagePanel(ParticleEditor.this, "Preview Image", "");
+		addEditorRow(previewImagePanel);
 
-				JPanel gridPanel = new JPanel(new GridLayout());
-				boolean previousSelected = renderGridCheckBox != null && renderGridCheckBox.isSelected();
-				renderGridCheckBox = new JCheckBox("Render Grid", previousSelected);
-				gridPanel.add(renderGridCheckBox, new GridBagConstraints());
-				addEditorRow(gridPanel);
-				addEditorRow(new CustomShadingPanel(ParticleEditor.this, "Shading", "Custom shader and multi-texture preview."));
+		JPanel gridPanel = new JPanel(new GridLayout());
+		boolean previousSelected = renderGridCheckBox != null && renderGridCheckBox.isSelected();
+		renderGridCheckBox = new JCheckBox("Render Grid", previousSelected);
+		gridPanel.add(renderGridCheckBox, new GridBagConstraints());
+		addEditorRow(gridPanel);
+		addEditorRow(new CustomShadingPanel(ParticleEditor.this, "Shading", "Custom shader and multi-texture preview."));
 
-				rowsPanel.removeAll();
-				ParticleEmitter emitter = getEmitter();
-				addRow(new ImagePanel(ParticleEditor.this, "Images", ""));
-				addRow(new CountPanel(ParticleEditor.this, "Count",
-					"Min number of particles at all times, max number of particles allowed."));
-				addRow(new RangedNumericPanel(emitter.getDelay(), "Delay",
-					"Time from beginning of effect to emission start, in milliseconds."));
-				addRow(new RangedNumericPanel(emitter.getDuration(), "Duration", "Time particles will be emitted, in milliseconds."));
-				addRow(new ScaledNumericPanel(emitter.getEmission(), "Duration", "Emission",
-					"Number of particles emitted per second."));
-				addRow(new ScaledNumericPanel(emitter.getLife(), "Duration", "Life", "Time particles will live, in milliseconds."));
-				addRow(new ScaledNumericPanel(emitter.getLifeOffset(), "Duration", "Life Offset",
-					"Particle starting life consumed, in milliseconds."));
-				addRow(new RangedNumericPanel(emitter.getXOffsetValue(), "X Offset",
-					"Amount to offset a particle's starting X location, in world units."));
-				addRow(new RangedNumericPanel(emitter.getYOffsetValue(), "Y Offset",
-					"Amount to offset a particle's starting Y location, in world units."));
-				addRow(new SpawnPanel(ParticleEditor.this, emitter.getSpawnShape(), "Spawn", "Shape used to spawn particles."));
-				addRow(new ScaledNumericPanel(emitter.getSpawnWidth(), "Duration", "Spawn Width",
-					"Width of the spawn shape, in world units."));
-				addRow(new ScaledNumericPanel(emitter.getSpawnHeight(), "Duration", "Spawn Height",
-					"Height of the spawn shape, in world units."));
-				addRow(new ScaledNumericPanel(emitter.getXScale(), "Life", "X Size", "Particle x size, in world units. If Y Size is not active, this also controls the y size"));
-				addRow(new ScaledNumericPanel(emitter.getYScale(), "Life", "Y Size", "Particle y size, in world units."));
-				addRow(new ScaledNumericPanel(emitter.getVelocity(), "Life", "Velocity", "Particle speed, in world units per second."));
-				addRow(new ScaledNumericPanel(emitter.getAngle(), "Life", "Angle", "Particle emission angle, in degrees."));
-				addRow(new ScaledNumericPanel(emitter.getRotation(), "Life", "Rotation", "Particle rotation, in degrees."));
-				addRow(new ScaledNumericPanel(emitter.getWind(), "Life", "Wind", "Wind strength, in world units per second."));
-				addRow(new ScaledNumericPanel(emitter.getGravity(), "Life", "Gravity", "Gravity strength, in world units per second."));
-				addRow(new GradientPanel(emitter.getTint(), "Tint", "", false));
-				addRow(new PercentagePanel(emitter.getTransparency(), "Life", "Transparency", ""));
-				addRow(new OptionsPanel(ParticleEditor.this, "Options", ""));
-				for (Component component : rowsPanel.getComponents())
-					if (component instanceof EditorPanel) ((EditorPanel)component).update(ParticleEditor.this);
-				rowsPanel.repaint();
-			}
-		});
+		rowsPanel.removeAll();
+		ParticleEmitter emitter = getEmitter();
+		addRow(new ImagePanel(ParticleEditor.this, "Images", ""));
+		addRow(new CountPanel(ParticleEditor.this, "Count",
+			"Min number of particles at all times, max number of particles allowed."));
+		addRow(new RangedNumericPanel(emitter.getDelay(), "Delay",
+			"Time from beginning of effect to emission start, in milliseconds."));
+		addRow(new RangedNumericPanel(emitter.getDuration(), "Duration", "Time particles will be emitted, in milliseconds."));
+		addRow(new ScaledNumericPanel(emitter.getEmission(), "Duration", "Emission",
+			"Number of particles emitted per second."));
+		addRow(new ScaledNumericPanel(emitter.getLife(), "Duration", "Life", "Time particles will live, in milliseconds."));
+		addRow(new ScaledNumericPanel(emitter.getLifeOffset(), "Duration", "Life Offset",
+			"Particle starting life consumed, in milliseconds."));
+		addRow(new RangedNumericPanel(emitter.getXOffsetValue(), "X Offset",
+			"Amount to offset a particle's starting X location, in world units."));
+		addRow(new RangedNumericPanel(emitter.getYOffsetValue(), "Y Offset",
+			"Amount to offset a particle's starting Y location, in world units."));
+		addRow(new SpawnPanel(ParticleEditor.this, emitter.getSpawnShape(), "Spawn", "Shape used to spawn particles."));
+		addRow(new ScaledNumericPanel(emitter.getSpawnWidth(), "Duration", "Spawn Width",
+			"Width of the spawn shape, in world units."));
+		addRow(new ScaledNumericPanel(emitter.getSpawnHeight(), "Duration", "Spawn Height",
+			"Height of the spawn shape, in world units."));
+		addRow(new ScaledNumericPanel(emitter.getXScale(), "Life", "X Size", "Particle x size, in world units. If Y Size is not active, this also controls the y size"));
+		addRow(new ScaledNumericPanel(emitter.getYScale(), "Life", "Y Size", "Particle y size, in world units."));
+		addRow(new ScaledNumericPanel(emitter.getVelocity(), "Life", "Velocity", "Particle speed, in world units per second."));
+		addRow(new ScaledNumericPanel(emitter.getAngle(), "Life", "Angle", "Particle emission angle, in degrees."));
+		addRow(new ScaledNumericPanel(emitter.getRotation(), "Life", "Rotation", "Particle rotation, in degrees."));
+		addRow(new ScaledNumericPanel(emitter.getWind(), "Life", "Wind", "Wind strength, in world units per second."));
+		addRow(new ScaledNumericPanel(emitter.getGravity(), "Life", "Gravity", "Gravity strength, in world units per second."));
+		addRow(new GradientPanel(emitter.getTint(), "Tint", "", false));
+		addRow(new PercentagePanel(emitter.getTransparency(), "Life", "Transparency", ""));
+		addRow(new OptionsPanel(ParticleEditor.this, "Options", ""));
+		for (Component component : rowsPanel.getComponents())
+			if (component instanceof EditorPanel) ((EditorPanel)component).update(ParticleEditor.this);
+		rowsPanel.repaint();
 	}
 
 	void addEditorRow (JPanel row) {
@@ -427,11 +423,6 @@ public class ParticleEditor extends JFrame {
 			float[] colors = backgroundColor.getColors();
 			Gdx.gl.glClearColor(colors[0], colors[1], colors[2], 1.0f);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-			//TODO: lwjgl3 currently calls the render method too early
-			if(previewImagePanel==null){
-				return;
-			}
 
 			previewImagePanel.updateSpritePosition();
 
